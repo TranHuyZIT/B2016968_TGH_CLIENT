f04c7b6c0d07c4a022d066c460d969828f3c88b4
TranHuy2k2 <giahuytran2002@gmail.com>
2023-04-02 13:07:50 +0700

    updated contact views, implement form and update form

---
+++ src/components/ContactList.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/require-valid-default-prop -->
+<script>
+export default {
+  props: {
+    contacts: { type: Array, default: [] },
+    activeIndex: { type: Number, default: -1 }
+  },
+  emits: ['update:activeIndex'],
+  methods: {
+    updateActiveIndex(index) {
+      this.$emit('update:activeIndex', index)
+    }
+  }
+}
+</script>
+<template>
+  <ul class="list-group">
+    <li
+      class="list-group-item"
+      v-for="(contact, index) in contacts"
+      :key="contact._id"
+      :class="{ active: index === activeIndex }"
+      @click="updateActiveIndex(index)"
+    >
+      {{ contact.name }}
+    </li>
+  </ul>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,12 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3001
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true
+      }
+    }
   }
 })
